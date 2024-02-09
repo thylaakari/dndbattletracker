802d1668b263c12e0abe5752f2671210d484184b
Lev <thylaakari@yandex.ru>
2024-02-09 12:24:04 +0500

    Adding footer

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,16 @@
 			<add-hero></add-hero>
 			<hero-list></hero-list>
 		</v-main>
+		<v-footer border class="bg-grey-lighten-5" app>
+			<v-btn variant="plain" disabled>{{ new Date().getFullYear() }}</v-btn>
+			<v-btn variant="plain" disabled>Lev Vorobyov</v-btn>
+			<v-btn variant="text" href="https://vk.com/lev2die" target="_blank"
+				>VK</v-btn
+			>
+			<v-btn variant="text" href="https://github.com/thylaakari" target="_blank"
+				>GitHub</v-btn
+			>
+		</v-footer>
 	</v-app>
 </template>
 
